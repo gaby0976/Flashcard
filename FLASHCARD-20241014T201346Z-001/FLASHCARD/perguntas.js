7669266a2765d9f80f9e5f430677bd491f6b4b21
gaby0976 <144721641+gaby0976@users.noreply.github.com>
2024-10-14 17:48:01 -0300

    Add files via upload

---
+++ FLASHCARD-20241014T201346Z-001/FLASHCARD/perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Programação',
+    'O que é desenvolvimento web?',
+    'O desenvolvimento web é o processo de construir e manter sites e aplicações web, envolvendo tanto o front-end (parte visual) quanto o back-end (servidor e banco de dados).'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'O maior país do mundo é a Rússia, com uma área de aproximadamente 17,1 milhões de km²'
+)
+
+criaCartao(
+    'Programação',
+    'O que O que é um bug em programação?',
+    'Um bug é um erro ou falha em um programa que causa um comportamento inesperado ou indesejado.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a diferença entre "much" e "many"?',
+    'Much é usado com substantivos incontáveis (ex: much water), enquanto many é usado com substantivos contáveis (ex: many apples)'
+)
